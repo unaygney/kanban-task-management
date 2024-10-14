411fabb192afc542a2d22b1f52c1b291ffc1a044
Guney Unay <guneymarketing17@hotmail.com>
2024-10-14 18:47:33 +0300

    :sparkles: added dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 
+import { ThemeProvider } from '@/components/providers'
+
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -14,7 +16,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
