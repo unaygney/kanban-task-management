337ff6164a3e0bcd17ccc826362d912936d54580
Guney Unay <guneymarketing17@hotmail.com>
2024-10-21 19:31:40 +0300

    :globe_with_meridians: Internationalization and localization.

--- src/lib/intl.ts
+++ src/lib/intl.ts
@@ -1,4 +1,8 @@
+import { i18n } from '../../i18n-config'
 import { createIntl } from '@formatjs/intl'
+import { match as matchLocale } from '@formatjs/intl-localematcher'
+import Negotiator from 'negotiator'
+import { NextRequest } from 'next/server'
 import 'server-only'
 
 import type { Locale } from '@/lib/definitions'
@@ -8,4 +12,22 @@ export async function getIntl(locale: Locale) {
     locale: locale,
     messages: (await import(`../lang/${locale}.json`)).default,
   })
+}
+
+export function getLocale(request: NextRequest): string | undefined {
+  // Negotiator expects plain object so we need to transform headers
+  const negotiatorHeaders: Record<string, string> = {}
+  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
+
+  // @ts-expect-error locales are readonly
+  const locales: string[] = i18n.locales
+
+  // Use negotiator and intl-localematcher to get best locale
+  const languages = new Negotiator({ headers: negotiatorHeaders }).languages(
+    locales
+  )
+
+  const locale = matchLocale(languages, locales, i18n.defaultLocale)
+
+  return locale
 }
