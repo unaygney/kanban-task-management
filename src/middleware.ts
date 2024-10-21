b3ce8beb5ce8528faf9a05e40e1965205e484532
Guney Unay <guneymarketing17@hotmail.com>
2024-10-21 19:31:23 +0300

    :wrench: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,25 @@
-import { match as matchLocale } from '@formatjs/intl-localematcher'
-import Negotiator from 'negotiator'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { v4 as uuidv4 } from 'uuid'
 
 import { i18n } from './../i18n-config'
+import { getLocale } from './lib/intl'
 
-function getLocale(request: NextRequest): string | undefined {
-  // Negotiator expects plain object so we need to transform headers
-  const negotiatorHeaders: Record<string, string> = {}
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
-
-  // @ts-expect-error locales are readonly
-  const locales: string[] = i18n.locales
-
-  // Use negotiator and intl-localematcher to get best locale
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages(
-    locales
-  )
-
-  const locale = matchLocale(languages, locales, i18n.defaultLocale)
-
-  return locale
-}
-
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
+  const user_id = request.cookies.get('user_id')?.value ?? null
+
+  const response = NextResponse.next()
+
+  if (!user_id) {
+    response.cookies.set('user_id', uuidv4(), {
+      path: '/',
+      maxAge: 30 * 24 * 60 * 60,
+      sameSite: 'lax',
+      secure: true,
+      httpOnly: true,
+    })
+  }
 
   const fallbackPage = '/dashboard'
 
@@ -58,6 +53,8 @@ export function middleware(request: NextRequest) {
       new URL(`${sanitizedPathname}/${fallbackPage}`, request.url)
     )
   }
+
+  return response
 }
 
 export const config = {
