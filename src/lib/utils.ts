a988ad5c67ec780a437f763984f8e552b498c5c2
Guney Unay <guneymarketing17@hotmail.com>
2024-10-24 18:37:50 +0300

    :wrench: update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+export function generateRandomColor(): string {
+  let r = Math.floor(Math.random() * 256)
+  let g = Math.floor(Math.random() * 256)
+  let b = Math.floor(Math.random() * 256)
+
+  while ((r > 200 && g > 200 && b > 200) || (r < 50 && g < 50 && b < 50)) {
+    r = Math.floor(Math.random() * 256)
+    g = Math.floor(Math.random() * 256)
+    b = Math.floor(Math.random() * 256)
+  }
+
+  return `rgb(${r}, ${g}, ${b})`
 }
